80208faa9e60438b34b7fbb577fd9b314e192224
Волков Алексей <bigsmiler1991+github@gmail.com>
2024-06-23 15:55:40 +0300

    Created new Allure report for final project sprint 6

--- conftest.py
+++ conftest.py
@@ -1,25 +1,14 @@
 import pytest
 from selenium import webdriver
-import time
 
 
 @pytest.fixture(scope="function")
-# def driver():
-#     options = webdriver.FirefoxOptions()
-#     options.add_argument('--ignore-certificate-errors')
-#     options.add_argument('--ignore-ssl-errors')
-#     options.add_argument('-headless')
-#     firefox_driver = webdriver.Firefox(options=options)
-#     firefox_driver.set_window_size(1920, 1080)
-#     yield firefox_driver
-#     firefox_driver.quit()
-
-
 def driver():
-    options = webdriver.ChromeOptions()
+    options = webdriver.FirefoxOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
-    chrome_driver = webdriver.Chrome(options=options)
-    yield chrome_driver
-    time.sleep(3)
-    chrome_driver.close()
+    options.add_argument('-headless')
+    firefox_driver = webdriver.Firefox(options=options)
+    firefox_driver.set_window_size(1920, 1080)
+    yield firefox_driver
+    firefox_driver.quit()
